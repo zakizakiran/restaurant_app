2dcab723bb79445b7982ffef7f0a2fd276094338
zakizakiran <zzakiran4@gmail.com>
2023-12-13 17:24:15 +0700

    updated menu carousel

--- lib/pages/detail_restaurant_page.dart
+++ lib/pages/detail_restaurant_page.dart
@@ -22,7 +22,7 @@ class _DetailRestaurantPageState extends State<DetailRestaurantPage> {
         headerSliverBuilder: (context, innerBoxIsScrolled) {
           return [
             SliverAppBar(
-              surfaceTintColor: Colors.transparent,
+              surfaceTintColor: HexColor('F64363'),
               leading: Padding(
                 padding: const EdgeInsets.all(8.0),
                 child: ElevatedButton.icon(
@@ -40,15 +40,17 @@ class _DetailRestaurantPageState extends State<DetailRestaurantPage> {
                   label: const Text(''),
                 ),
               ),
-              backgroundColor: HexColor('F64363'),
               expandedHeight: 200,
               pinned: true,
               flexibleSpace: FlexibleSpaceBar(
                 background: Hero(
                   tag: widget.restaurant.pictureId,
-                  child: Image.network(
-                    widget.restaurant.pictureId,
-                    fit: BoxFit.cover,
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(18.0),
+                    child: Image.network(
+                      widget.restaurant.pictureId,
+                      fit: BoxFit.cover,
+                    ),
                   ),
                 ),
               ),
@@ -56,89 +58,95 @@ class _DetailRestaurantPageState extends State<DetailRestaurantPage> {
           ];
         },
         body: SingleChildScrollView(
-          child: Padding(
-            padding:
-                const EdgeInsets.symmetric(horizontal: 15.0, vertical: 20.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                const SizedBox(height: 15.0),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const SizedBox(height: 15.0),
+              Container(
+                padding: const EdgeInsets.symmetric(
+                    horizontal: 15.0, vertical: 20.0),
+                child: Column(
                   children: [
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       children: [
-                        Text(
-                          widget.restaurant.name,
-                          style: const TextStyle(
-                            fontSize: 22.0,
-                            fontWeight: FontWeight.bold,
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              widget.restaurant.name,
+                              style: const TextStyle(
+                                fontSize: 22.0,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            const SizedBox(height: 10.0),
+                            Row(
+                              children: [
+                                Icon(
+                                  CupertinoIcons.location_solid,
+                                  size: 15.0,
+                                  color: HexColor('F64363'),
+                                ),
+                                const SizedBox(width: 5.0),
+                                Text(widget.restaurant.city),
+                              ],
+                            ),
+                            const SizedBox(height: 8.0),
+                            Row(
+                              children: [
+                                const Icon(
+                                  CupertinoIcons.star_fill,
+                                  size: 15.0,
+                                  color: Colors.amber,
+                                ),
+                                const SizedBox(width: 5.0),
+                                Text(widget.restaurant.rating.toString())
+                              ],
+                            ),
+                          ],
+                        ),
+                        IconButton(
+                          icon: Icon(
+                            isLoved ? Icons.favorite : Icons.favorite_border,
+                            color: isLoved ? HexColor('F64363') : null,
                           ),
-                        ),
-                        const SizedBox(height: 10.0),
-                        Row(
-                          children: [
-                            Icon(
-                              CupertinoIcons.location_solid,
-                              size: 15.0,
-                              color: HexColor('F64363'),
-                            ),
-                            const SizedBox(width: 5.0),
-                            Text(widget.restaurant.city),
-                          ],
-                        ),
-                        const SizedBox(height: 8.0),
-                        Row(
-                          children: [
-                            const Icon(
-                              CupertinoIcons.star_fill,
-                              size: 15.0,
-                              color: Colors.amber,
-                            ),
-                            const SizedBox(width: 5.0),
-                            Text(widget.restaurant.rating.toString())
-                          ],
-                        ),
+                          onPressed: () {
+                            setState(() {
+                              isLoved = !isLoved;
+                            });
+                          },
+                        )
                       ],
                     ),
-                    IconButton(
-                      icon: Icon(
-                        isLoved ? Icons.favorite : Icons.favorite_border,
-                        color: isLoved ? HexColor('F64363') : null,
+                    ReadMoreText(
+                      widget.restaurant.description,
+                      trimLines: 5,
+                      trimMode: TrimMode.Line,
+                      trimCollapsedText: 'Read more',
+                      trimExpandedText: 'Less',
+                      moreStyle: TextStyle(
+                        fontSize: 12.0,
+                        fontWeight: FontWeight.w600,
+                        color: HexColor(
+                          'F64363',
+                        ),
                       ),
-                      onPressed: () {
-                        setState(() {
-                          isLoved = !isLoved;
-                        });
-                      },
-                    )
+                      lessStyle: TextStyle(
+                        fontSize: 12.0,
+                        fontWeight: FontWeight.w600,
+                        color: HexColor(
+                          'F64363',
+                        ),
+                      ),
+                    ),
                   ],
                 ),
-                const SizedBox(height: 15.0),
-                ReadMoreText(
-                  widget.restaurant.description,
-                  trimLines: 5,
-                  trimMode: TrimMode.Line,
-                  trimCollapsedText: 'Read more',
-                  trimExpandedText: 'Less',
-                  moreStyle: TextStyle(
-                    fontSize: 12.0,
-                    fontWeight: FontWeight.w600,
-                    color: HexColor(
-                      'F64363',
-                    ),
-                  ),
-                  lessStyle: TextStyle(
-                    fontSize: 12.0,
-                    fontWeight: FontWeight.w600,
-                    color: HexColor(
-                      'F64363',
-                    ),
-                  ),
-                ),
-                const SizedBox(height: 50.0),
-                Row(
+              ),
+              const SizedBox(height: 50.0),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 15.0),
+                child: Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
                     const Text(
@@ -154,38 +162,41 @@ class _DetailRestaurantPageState extends State<DetailRestaurantPage> {
                     )
                   ],
                 ),
-                const SizedBox(height: 10.0),
-                SizedBox(
-                  height: MediaQuery.of(context).size.height / 10,
-                  child: ListView.builder(
-                    scrollDirection: Axis.horizontal,
-                    itemCount: widget.restaurant.foods.length,
-                    itemBuilder: (BuildContext context, int index) {
-                      final food = widget.restaurant.foods[index];
-                      return Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 4.0),
-                        child: SizedBox(
-                          width: MediaQuery.of(context).size.width / 2,
-                          child: Card(
-                            color: HexColor('F3E7E9'),
-                            child: Center(
-                              child: Text(
-                                food.name,
-                                textAlign: TextAlign.center,
-                                style: TextStyle(
-                                  color: HexColor('F64363'),
-                                  fontSize: 12.0,
-                                ),
+              ),
+              const SizedBox(height: 10.0),
+              SizedBox(
+                height: MediaQuery.of(context).size.height / 10,
+                child: ListView.builder(
+                  scrollDirection: Axis.horizontal,
+                  itemCount: widget.restaurant.foods.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    final food = widget.restaurant.foods[index];
+                    return Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                      child: SizedBox(
+                        width: MediaQuery.of(context).size.width / 2,
+                        child: Card(
+                          color: HexColor('F3E7E9'),
+                          child: Center(
+                            child: Text(
+                              food.name,
+                              textAlign: TextAlign.center,
+                              style: TextStyle(
+                                color: HexColor('F64363'),
+                                fontSize: 12.0,
                               ),
                             ),
                           ),
                         ),
-                      );
-                    },
-                  ),
+                      ),
+                    );
+                  },
                 ),
-                const SizedBox(height: 30.0),
-                Row(
+              ),
+              const SizedBox(height: 30.0),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 15.0),
+                child: Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
                     const Text(
@@ -201,38 +212,38 @@ class _DetailRestaurantPageState extends State<DetailRestaurantPage> {
                     )
                   ],
                 ),
-                const SizedBox(height: 10.0),
-                SizedBox(
-                  height: MediaQuery.of(context).size.height / 10,
-                  child: ListView.builder(
-                    scrollDirection: Axis.horizontal,
-                    itemCount: widget.restaurant.drinks.length,
-                    itemBuilder: (BuildContext context, int index) {
-                      final drink = widget.restaurant.drinks[index];
-                      return Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 4.0),
-                        child: SizedBox(
-                          width: MediaQuery.of(context).size.width / 2,
-                          child: Card(
-                            color: HexColor('F3E7E9'),
-                            child: Center(
-                              child: Text(
-                                drink.name,
-                                textAlign: TextAlign.center,
-                                style: TextStyle(
-                                  fontSize: 12.0,
-                                  color: HexColor('F64363'),
-                                ),
+              ),
+              const SizedBox(height: 10.0),
+              SizedBox(
+                height: MediaQuery.of(context).size.height / 10,
+                child: ListView.builder(
+                  scrollDirection: Axis.horizontal,
+                  itemCount: widget.restaurant.drinks.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    final drink = widget.restaurant.drinks[index];
+                    return Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                      child: SizedBox(
+                        width: MediaQuery.of(context).size.width / 2,
+                        child: Card(
+                          color: HexColor('F3E7E9'),
+                          child: Center(
+                            child: Text(
+                              drink.name,
+                              textAlign: TextAlign.center,
+                              style: TextStyle(
+                                fontSize: 12.0,
+                                color: HexColor('F64363'),
                               ),
                             ),
                           ),
                         ),
-                      );
-                    },
-                  ),
+                      ),
+                    );
+                  },
                 ),
-              ],
-            ),
+              ),
+            ],
           ),
         ),
       ),
